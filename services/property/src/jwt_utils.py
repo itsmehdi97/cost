f12f4245db0b7fe7403901a5e53a26b1506a1b51
mehdi <kamani@ipreez.ir>
2021-10-16 09:27:53 +0330

    validate auth token

--- services/property/src/jwt_utils.py
+++ services/property/src/jwt_utils.py
@@ -1,10 +1,18 @@
+import json
+from datetime import datetime
+
 from jose import jws
 from jose.utils import base64url_decode
-import json
 
+
+def is_expired(ts):
+    return datetime.now().timestamp() > ts
 
 def verify_token(token, secret):
     encoded_header, _, _ = token.split('.')
     header = json.loads(base64url_decode(encoded_header).decode())
     bytes_payload = jws.verify(token, secret, algorithms=[header['alg']])
-    return json.loads(bytes_payload.decode())
+    payload = json.loads(bytes_payload.decode())
+    if is_expired(payload.get('exp')):
+        return None
+    return payload
