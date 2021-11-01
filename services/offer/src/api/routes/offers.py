f7fc0f5439d7c78e3b0a00e69e88d03241ba65bf
mehdi <kamani@ipreez.ir>
2021-11-01 13:53:38 +0330

    publish offer place/update/cancel messages

--- services/offer/src/api/routes/offers.py
+++ services/offer/src/api/routes/offers.py
@@ -25,14 +25,17 @@ async def get_offer(
 @router.post("/place", response_model=schemas.Offer)
 async def place_offer(
         offer: schemas.OfferCreate,
-        # bg_tasks: BackgroundTasks,
+        bg_tasks: BackgroundTasks,
         offers_svc: OfferService = Depends(get_service(Service_type=OfferService, Repo_type=OfferRepository)),
-        user: schemas.User = Depends(current_user)
+        user: schemas.User = Depends(current_user),
+        publisher: Publisher = Depends(get_publisher)
     ):
     db_offer = await offers_svc.place(offer=offer, requesting_user=user)
-
-    # bg_tasks.add_task(
-    #     tasks.offer_placed, rabbit_conn, schemas.Offer.parse_obj(db_offer.__dict__))
+    bg_tasks.add_task(
+        publisher.publish,
+        body=schemas.Offer.parse_obj(db_offer.__dict__).json(),
+        routing_key='offer.place',
+        exchange='offers')
 
     return db_offer
 
@@ -40,15 +43,19 @@ async def place_offer(
 @router.put("/update/{offer_id}", response_model=schemas.Offer)
 async def update_offer(
         offer_id: int,
-        # bg_tasks: BackgroundTasks,
+        bg_tasks: BackgroundTasks,
         offer: schemas.OfferUpdate,
         offers_svc: OfferService = Depends(get_service(Service_type=OfferService, Repo_type=OfferRepository)),
-        user: schemas.User = Depends(current_user)
+        user: schemas.User = Depends(current_user),
+        publisher: Publisher = Depends(get_publisher)
     ):
     db_offer = await offers_svc.update(id=offer_id, offer=offer, requesting_user=user)
-
-    # bg_tasks.add_task(
-    #     tasks.offer_updated, rabbit_conn, schemas.Offer.parse_obj(db_offer.__dict__))
+    if db_offer:
+        bg_tasks.add_task(
+                publisher.publish,
+                body=schemas.Offer.parse_obj(db_offer.__dict__).json(),
+                routing_key='offer.update',
+                exchange='offers')
 
     return db_offer
 
