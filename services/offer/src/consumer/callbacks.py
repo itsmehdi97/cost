903782f65290c5bd71fd2781f13adc8acb52f195
mehdi <kamani@ipreez.ir>
2021-11-07 19:12:19 +0330

    simple consumer setup

---
+++ services/offer/src/consumer/callbacks.py
@@ -0,0 +1,20 @@
+from aio_pika import IncomingMessage
+
+from db.repositories.prop import PropRepository
+import schemas
+
+
+async def on_prop_msg(repo: PropRepository, msg: IncomingMessage):
+    msg_type = msg.routing_key.split('.')[1]
+
+    print('## prop msg rec', msg.body)
+
+    if msg_type == 'add':
+        new_prop = schemas.PropCreate.parse_raw(msg.body)
+        await repo.create(prop=new_prop)
+        msg.ack()
+
+    if msg_type == 'update':
+        update_prop = schemas.PropUpdate.parse_ras(msg.body)
+        await repo.update(prop=update_prop)
+        msg.ack()
--- services/offer/src/core/tasks.py
+++ services/offer/src/core/tasks.py
@@ -4,13 +4,14 @@ from fastapi import FastAPI
 
 from db.tasks import connect_to_db, close_db_connection
 from broker.tasks import connect_to_broker, close_broker_connection
-
+from consumer.tasks import init_consumers
 
 
 def create_start_app_handler(app: FastAPI) -> Callable:
     async def start_app() -> None:
         connect_to_db(app=app)
         await connect_to_broker(app)
+        await init_consumers(app)
 
     return start_app
 
