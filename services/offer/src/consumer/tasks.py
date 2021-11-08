e973a560cd6cb27cb01e83c9e3f4c016bfdd3850
mehdi <kamani@ipreez.ir>
2021-11-08 11:45:43 +0330

    pass repo instance instead of type

--- services/offer/src/consumer/tasks.py
+++ services/offer/src/consumer/tasks.py
@@ -6,12 +6,16 @@ from consumer.callbacks import on_prop_msg
 
 
 async def start_props_consumer(app):
-    await app.state._consumer.start_consuming(
-        PropRepository,
-        exchange='props',
-        binding_key='prop.*',
-        cb=on_prop_msg,
-        queue='prop_changes')
+    db = app.state._db()
+    try:
+        await app.state._consumer.start_consuming(
+            PropRepository(db),
+            exchange='props',
+            binding_key='prop.*',
+            cb=on_prop_msg,
+            queue='prop_changes')
+    finally:
+        db.close()
 
 async def init_consumers(app):
     app.state._consumer = BaseConsumer(broker_conn=app.state._broker_conn)
